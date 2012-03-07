c4fd84acc2773948a026b58a1fc597b9cdbb957b
Daniel P Williams <dwilliams@port8080.net>
2012-03-06 21:44:41 -0700

    Adding previous modifications for the post archive feature.
    Signed-off-by: Daniel P Williams <dwilliams@port8080.net>

--- application/views/home/page_body_archive.php
+++ application/views/home/page_body_archive.php
@@ -1,7 +1,7 @@
 <?php
   /*** 
-    *  page_body_main.php
-    *  2009.03.08
+    *  page_body_archive.php
+    *  2011.12.26
     *  http://www.port8080.net/
     *  Daniel Patrick Williams
     *  dwilliams@port8080.net
@@ -10,19 +10,26 @@
   // Takes array to populate a blog like entry
   // ARRAY ROW LAYOUT: (each row is an entry)
   // [ <post_title>, <post_body>, <post_date> ]
-  // The array is called $array_posts[##][title,post,date]
+  // The array is called $array_posts[##][post_title, post_body, post_date]
   
   echo '    <div id="page_main">' . "\n";
   foreach ($array_posts as $post) {
     echo '      <div class="main_entry">' . "\n";
+    // post title: use date for front page
     echo '        <div class="entry_title">' . "\n";
-    echo '          ' . $post['post_title'] . "\n"; // post title: use date for front page
+    echo '          ';
+    echo anchor('/home/post/' . $post['id'], $post['post_title']);
+    echo "\n";
     echo '        </div>' . "\n";
+    // post body
     echo '        <div class="entry_post">' . "\n";
-    echo '          ' . $post['post_body'] . "\n";  // post body
+    echo '          ' . substr($post['post_body'], 0, 256) . '...' . "\n";
+    echo '          <br />' . "\n";
+    echo '          ' . anchor('/home/post/' . $post['id'], '(Read more...)') . "\n";
     echo '        </div>' . "\n";
+    // post date
     echo '        <div class="entry_date">' . "\n";
-    echo '          ' . $post['post_date'] . "\n";  // post date
+    echo '          ' . $post['post_date'] . "\n";
     echo '        </div>' . "\n";
     echo '      </div>' . "\n";
   }
