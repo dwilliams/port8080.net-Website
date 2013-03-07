146837418636ec35a7942b6ca39d33e9fc7127e1
Daniel Williams <dwilliams@port8080.net>
2013-03-07 13:14:18 -0700

    Adding rest api for port8080.  The rest api will eventually have functions for each of the sections of the site.

---
+++ application/controllers/rest.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+  /***
+    *  rest.php
+    *  2013.03.07
+    *  http://www.port8080.net
+    *  Daniel Patrick Williams
+    *  dwilliams@port8080.net
+    *  Updated: 2013.03.07
+   ***/
+  
+  // ReSTful API for Port8080.net
+  
+  // Using the library by philsturgeon available at:
+  // https://github.com/philsturgeon/codeigniter-restserver
+  require(APPPATH . 'libraries/REST_Controller.php');
+  
+  class Rest extends REST_Controller {
+  }
+
+/* DPW */
+/* END OF CODE */
